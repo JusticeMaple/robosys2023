eab123bd803b9236e9e0633517dca0bebfe69cd4
Tei <justice_zwy@yahoo.co.jp>
2023-11-02 13:23:44 +0900

    add change

--- test.bash
+++ test.bash
@@ -1,22 +1,12 @@
 #!/bin/bash
-
 ng () {
-	echo NG at Line ${1}
-	ret=1
+      echo NG at Line $1
+      res=1
 }
-
 ret=0
+### I/O TEST ###
 out=$(seq 5 | ./plus_stdin)
-[ "${out}" = 15 ] || ng "${LINENO}"
+[ "${out}" = 14 ] || ng ${LINENO}
 
-### 変なinput ###
-out=$(echo あ | ./plus_stdin)
-[ "$?" = 1 ] || ng "${LINENO}"
-[ "${out}" = "" ] || ng "${LINENO}"
-
-out=$(echo | ./plus_stdin)
-[ "$?" = 1 ] || ng "${LINENO}"
-[ "${out}" = "" ] || ng "${LINENO}"
 [ "$ret" = 0 ] && echo OK
-
 exit $ret
