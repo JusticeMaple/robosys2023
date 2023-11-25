afc82b713e55bfc0604db9ea90b9a1737b4a7f49
Tei <justice_zwy@yahoo.co.jp>
2023-11-25 21:32:05 +0900

    Just test

--- test.bash
+++ test.bash
@@ -1,14 +1,13 @@
 #!/bin/bash
 # SPDX-FileCopyrightText: 2023 Yunyuan Zheng
 # SPDX-License-Identifier: BSD-3-Clause
+
 ng () {
 	echo NG at Line $1
 	res=1
 }
 
 res=0
-
-### I/O TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
