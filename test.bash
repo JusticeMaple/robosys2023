cb6f3b2ad9f64c30d2824980cb4899870cbf8c91
Tei <justice_zwy@yahoo.co.jp>
2023-11-25 20:48:33 +0900

    Your commit message

--- test.bash
+++ test.bash
@@ -1,12 +1,3 @@
 #!/bin/bash
-ng () {
-      echo NG at Line $1
-      res=1
-}
-ret=0
-### I/O TEST ###
-out=$(seq 5 | ./plus_stdin)
-[ "${out}" = 14 ] || ng ${LINENO}
 
-[ "$ret" = 0 ] && echo OK
-exit $ret
+seq 5 | ./plus
